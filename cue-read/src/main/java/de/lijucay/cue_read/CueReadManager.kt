20a9a446f13d24681f9d050b8bea8601972e8281
Lijucay <lijucayy@proton.me>
2026-05-21 15:15:57 +0200

    Update NFC extra retrieval for Android 13 type-safety

--- cue-read/src/main/java/de/lijucay/cue_read/CueReadManager.kt
+++ cue-read/src/main/java/de/lijucay/cue_read/CueReadManager.kt
@@ -4,20 +4,34 @@ import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.Tag
+import android.os.Build
 
 class CueReadManager(
     private val nfcReadManager: NfcReadManager = NfcReadManager()
 ) {
     fun read(intent: Intent): ReadResult {
-        val rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+        val rawMessages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES,
+                NdefMessage::class.java
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+        }
+
         if (!rawMessages.isNullOrEmpty()) {
             val ndefMessage = rawMessages[0] as NdefMessage
 
             return nfcReadManager.read(ndefMessage)
         }
 
-        val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-            ?: return ReadResult.EmptyTag
+        val tag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(NfcAdapter.EXTRA_TAG, Tag::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        } ?: return ReadResult.EmptyTag
 
         return nfcReadManager.read(tag)
     }
